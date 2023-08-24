50272f9d06b4426692d166b42189b2d77da8f75c
mujee7 <mujtabarehman1444@gmail.com>
2023-08-24 19:40:17 +0500

    remove

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const cors=require('cors')
 const mongoose=require('mongoose');
 const express=require('express')
-const stripe = require('stripe')(process.env.SECRET_KEY);
 const app=express();
 
 
 app.get('/',async (req,res)=>{
     
     res.json({"message":"hello revcieved"})
+    console.log("webhook worked")
    
 })
 
